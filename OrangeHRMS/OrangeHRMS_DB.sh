23d35ab8b670248920e6d3fe7cc2584e78db30da
Ellipsonic <developer@ellipsonic.com>
2015-07-24 08:44:42 +0530

    Changed IDENTIFIED by 'root' to 'admin'

--- OrangeHRMS/OrangeHRMS_DB.sh
+++ OrangeHRMS/OrangeHRMS_DB.sh
@@ -35,7 +35,7 @@ mysql -u root orangehrm2 < /tmp/orangehrm_mysql.sql -padmin
  
 # Allow any server to connect 
 sed -i "s/bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
-echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION" | mysql -u root -padmin
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'admin' WITH GRANT OPTION" | mysql -u root -padmin
 
 #Restart all the installed services to verify that everything is installed properly 
 echo -e "\n"
